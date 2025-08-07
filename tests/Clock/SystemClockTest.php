1f77f8fb55d3742793de15d2cc51761148e45604
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-08 00:45:56 +0200

    Add test coverage for System* clocks

---
+++ tests/Clock/SystemClockTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clock;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Recruiter\Clock\SystemClock;
+
+#[CoversClass(SystemClock::class)]
+class SystemClockTest extends TestCase
+{
+    public function testItReturnsCurrentTime(): void
+    {
+        $clock = new SystemClock();
+        $before = new \DateTimeImmutable();
+        $result = $clock->now();
+        $after = new \DateTimeImmutable();
+
+        $this->assertGreaterThanOrEqual($before, $result);
+        $this->assertLessThanOrEqual($after, $result);
+    }
+}
