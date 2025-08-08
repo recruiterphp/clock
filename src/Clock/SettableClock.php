34b3b4230b500ac88e2ed658fd9c9495ad89c4ac
D. Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-08 21:29:42 +0200

    Make SettableClock Symfony-compliant

---
+++ src/Clock/SettableClock.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Recruiter\Clock;
+
+use Symfony\Component\Clock\ClockInterface;
+
+class SettableClock extends AbstractClock
+{
+    private ?ManualClock $fixed = null;
+
+    public function __construct(private ClockInterface $wrapped)
+    {
+    }
+
+    public function now(): \DateTimeImmutable
+    {
+        return $this->enabledClock()->now();
+    }
+
+    public function nowIs(\DateTimeImmutable $fixed): void
+    {
+        $this->fixed = new ManualClock($fixed);
+    }
+
+    public function elapse(\DateInterval $amount): \DateTimeImmutable
+    {
+        $this->nowIs($this->now()->add($amount));
+
+        return $this->now();
+    }
+
+    public function reset(): void
+    {
+        $this->fixed = null;
+    }
+
+    public function sleep(float|int $seconds): void
+    {
+        $this->enabledClock()->sleep($seconds);
+    }
+
+
+    private function enabledClock(): ClockInterface
+    {
+        return $this->fixed ?? $this->wrapped;
+    }
+
+    /**
+     * @throws \DateInvalidTimeZoneException
+     */
+    public function withTimeZone(\DateTimeZone|string $timezone): static
+    {
+        $clone = clone $this;
+
+        $clone->wrapped = $this->wrapped->withTimeZone($timezone);
+        if ($clone->fixed) {
+            $clone->fixed = $clone->fixed->withTimeZone($timezone);
+        }
+
+        return $clone;
+    }
+}
---
+++ tests/Clock/SettableClockTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Recruiter\Clock;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Clock\ClockInterface;
+
+#[CoversClass(SettableClock::class)]
+class SettableClockTest extends TestCase
+{
+    private ClockInterface&MockObject $innerClock;
+    private SettableClock $clock;
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $this->innerClock = $this->createMock(ClockInterface::class);
+        $this->clock = new SettableClock($this->innerClock);
+    }
+
+    public function testCurrentShouldReturnStubbedTime(): void
+    {
+        $time = new \DateTimeImmutable('2015-02-01 10:00 UTC');
+        $this->clock->nowIs($time);
+
+        $this->assertEquals(
+            $time,
+            $this->clock->now(),
+        );
+    }
+
+    public function testCurrentShouldBeAskedToInnerClockIfNotSet(): void
+    {
+        $time = new \DateTimeImmutable('2015-02-01 10:00');
+
+        $this->innerClock
+            ->expects($this->any())
+            ->method('now')
+            ->willReturn($time)
+        ;
+
+        $this->assertEquals(
+            $time,
+            $this->clock->now(),
+        );
+    }
+
+    public function testStubbedTimeCanBeReset(): void
+    {
+        $time = new \DateTimeImmutable('2015-02-01 10:00');
+
+        $this->innerClock
+            ->expects($this->any())
+            ->method('now')
+            ->willReturn($time)
+        ;
+
+        $this->clock->nowIs(new \DateTimeImmutable('1985-05-21 08:40'));
+
+        $this->clock->reset();
+
+        $this->assertEquals($time, $this->clock->now());
+    }
+
+    public function testElapseAdvancesTimeAndReturnsNewTime(): void
+    {
+        $initialTime = new \DateTimeImmutable('2015-02-01 10:00:00');
+        $this->clock->nowIs($initialTime);
+
+        $interval = new \DateInterval('PT1H'); // 1 hour
+        $result = $this->clock->elapse($interval);
+
+        $expectedTime = new \DateTimeImmutable('2015-02-01 11:00:00');
+        $this->assertEquals($expectedTime, $result);
+        $this->assertEquals($expectedTime, $this->clock->now());
+    }
+
+    public function testSleep(): void
+    {
+        $initialTime = new \DateTimeImmutable('2015-02-01 10:00:00');
+        $this->clock->nowIs($initialTime);
+
+        $this->clock->sleep(1);
+
+        $expectedTime = new \DateTimeImmutable('2015-02-01 10:00:01');
+        $this->assertEquals($expectedTime, $this->clock->now());
+    }
+}
