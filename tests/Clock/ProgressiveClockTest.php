1a30929b564bf9504525a2ed2e58333967790ca5
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-08 22:26:31 +0200

    Add clarifying test

--- tests/Clock/ProgressiveClockTest.php
+++ tests/Clock/ProgressiveClockTest.php
@@ -23,6 +23,15 @@ class ProgressiveClockTest extends ClockTestCase
         $this->assertGreaterThan($this->clock->now(), $this->clock->now());
     }
 
+    public function testSleep(): void
+    {
+        $before = $this->clock->now(); // this advances the clock by 1 second after returning the current time
+        $this->clock->sleep(1);
+        $after = $this->clock->now();
+
+        $this->assertEquals($before->add(new \DateInterval('PT2S')), $after);
+    }
+
     public function testForwardInTimeAdvancesTheClock(): void
     {
         $before = $this->clock->now();
