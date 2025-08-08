07471020f1e53ec249d5aa81f5ba6eb5bbc2c02d
D. Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-08 20:13:07 +0200

    Cover trait in FixedClockTest

--- tests/Clock/FixedClockTest.php
+++ tests/Clock/FixedClockTest.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace Clock;
 
+use Recruiter\DateTime\UTCDateTime;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
 use Recruiter\Clock\FixedClock;
+use Recruiter\Clock\PsrMicrotimeClock;
+use Recruiter\Clock\PsrUTCClock;
 
 #[CoversClass(FixedClock::class)]
+#[UsesClass(PsrMicrotimeClock::class)]
+#[UsesClass(PsrUTCClock::class)]
+#[UsesClass(UTCDateTime::class)]
 class FixedClockTest extends TestCase
 {
     public function testItReturnsFixedTime(): void
@@ -55,6 +62,30 @@ class FixedClockTest extends TestCase
             new \DateTimeImmutable('2023-10-01 12:00:00', new \DateTimeZone('UTC')),
             $clock->now(),
             'Clock should return the fixed time from ISO 8601 string',
+        );
+    }
+
+    public function testConversionToMicrotime(): void
+    {
+        $fixedTime = new \DateTimeImmutable('2023-10-01 12:00:00.123456');
+        $clock = new FixedClock($fixedTime)->asMicrotime();
+
+        $this->assertEquals(
+            1696161600.123456,
+            $clock->current(),
+            'Clock should convert fixed time to microtime correctly',
+        );
+    }
+
+    public function testConversionToUTC(): void
+    {
+        $fixedTime = new \DateTimeImmutable('2023-10-01 12:00:00', new \DateTimeZone('Europe/Berlin'));
+        $clock = new FixedClock($fixedTime)->asUTC();
+
+        $this->assertEquals(
+            UTCDateTime::fromString('2023-10-01 10:00:00'),
+            $clock->current(),
+            'Clock should convert fixed time to UTC correctly',
         );
     }
 }
